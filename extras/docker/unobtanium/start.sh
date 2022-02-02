0e8724679edaed1bb8c851f2689c7635330e8b18
terhnt <stterhnt@gmail.com>
2022-02-02 17:51:37 +1100

    fix: unobtanium docker

--- extras/docker/unobtanium/start.sh
+++ extras/docker/unobtanium/start.sh
@@ -4,7 +4,11 @@
 # http://veithen.github.io/2014/11/16/sigterm-propagation.html
 : ${PARAMS:=""}
 trap 'kill -TERM $PID' TERM INT
-/usr/local/bin/unobtaniumd ${PARAMS} $@ &
+if [ ! -f /root/.unobtanium/blocks/blk00000.dat ]; then
+    /usr/local/bin/unobtaniumd -reindex ${PARAMS} $@ &
+else
+    /usr/local/bin/unobtaniumd ${PARAMS} $@ &
+fi
 PID=$!
 wait $PID
 trap - TERM INT
