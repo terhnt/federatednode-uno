1c0a22b0b80e092838a51a8bda05909f03cefbcf
Andrew <95168952+terhnt@users.noreply.github.com>
2022-02-06 15:46:30 +1100

    Fix reindex for testnet
    It now checks if config contains testnet, if it does it checks the testnet3 path for blk00000.dat if not there will reindex.
    previously testnet was checking the mainnet blk00000.dat path and wasn't reindexing if unobtanium mainnet allready started.

--- extras/docker/unobtanium/start.sh
+++ extras/docker/unobtanium/start.sh
@@ -4,7 +4,15 @@
 # http://veithen.github.io/2014/11/16/sigterm-propagation.html
 : ${PARAMS:=""}
 trap 'kill -TERM $PID' TERM INT
-if [ ! -f /root/.unobtanium/blocks/blk00000.dat ]; then
+
+PATH='/root/.unobtanium/blocks/blk00000.dat'
+
+# Check if testnet or not
+if [[ "$PARAMS" == *"testnet"* ]]; then
+    PATH='/root/.unobtanium/testnet3/blocks/blk00000.dat'
+fi
+
+if [ ! -f ${PATH} ]; then
     /usr/local/bin/unobtaniumd -reindex ${PARAMS} $@ &
 else
     /usr/local/bin/unobtaniumd ${PARAMS} $@ &
